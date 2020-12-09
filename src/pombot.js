b96e742171ec380fdddede921027c32613c9d8b5
Villa, Jeremy <VillaJ@valassis.com>
2020-12-09 16:00:55 -0500

    Added pom prompts

--- src/pombot.js
+++ src/pombot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const config = require("../config.json");
 const client = new Discord.Client();
+const fs = require("fs");
 const prefix = "!";
 
 client.on("message", function(message) {
@@ -21,6 +22,39 @@ client.on("message", function(message) {
     const sum = numArgs.reduce((counter, x) => counter += x);
     message.reply(`The sum of all the arguments you provided is ${sum}!`);
   }
+
+  else if (command === "start") {
+    const timeMins = args;
+    message.reply(`You have begun a Pom-session for ${timeMins} minute(s). Good luck!`);
+
+    setTimeout(() => {
+      message.reply(`Pom finished!`);
+
+      let filter = m => m.author.id === message.author.id
+      message.reply(`How did it go?`).then(() => {
+        message.channel.awaitMessages(filter, {
+            max: 1,
+            time: 30000,
+            errors: ['time']
+          })
+          .then(message => {
+            message = message.first()
+            message.reply(`You said, ${message.content}`);
+          })
+          .catch(collected => {
+              message.channel.send('Timeout');
+          });
+      })
+    }, args * 6000);
+    
+
+  }
+
+  function pomComplete(message) {
+    message.reply(`You have completed a Pom-session. Good luck!`);
+  }
 });
+
+
 
 client.login(config.BOT_TOKEN);
